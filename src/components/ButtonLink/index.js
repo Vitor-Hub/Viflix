024cd64c03ac51cf1a8d4d34ccde4631e5a1cff1
Vitor-Hub <rj.vitorsantos@gmail.com>
2020-07-30 22:11:37 -0300

    header done

--- src/components/ButtonLink/index.js
+++ src/components/ButtonLink/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
-
-const Button = styled.a`
-    background-color: #fff;
-    width: 100%;
-    border-radius: 5px;
-    margin-left: auto;
-`
-
-
-
+import PropTypes from 'prop-types';
 
 function ButtonLink(props) {
     // props => { className: "o que alguém passar" }
@@ -20,5 +10,16 @@ function ButtonLink(props) {
         </a>
     );
 }
+
+ButtonLink.defaultProps = {
+    href: '/',
+    className: '',
+};
+  
+ButtonLink.propTypes = {
+    className: PropTypes.string,
+    href: PropTypes.string,
+    children: PropTypes.node.isRequired,
+};
 
 export default ButtonLink;
