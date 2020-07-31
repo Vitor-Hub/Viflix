4ce616589f4447a8d31e937fecc5cd2537190480
Vitor-Hub <rj.vitorsantos@gmail.com>
2020-07-31 10:50:05 -0300

    body and head

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
 import Carousel from './components/Carousel';
+import Header from './components/Header';
 
 export default function App(){
     return (
         <>
             <div>
+                <Header />
                 <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
                             url= {dadosIniciais.categorias[0].videos[0].url}
                             videoDescription= {"O que é Front-end?"}
